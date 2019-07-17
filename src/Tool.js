bde4ac833479fdcbcddf1af0ea03d9be47bb9d3b
jordandde <ja3desou@uwaterloo.ca>
2019-07-17 11:01:30 -0400

    finished component

--- src/Tool.js
+++ src/Tool.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Tool.css";
 import test from "./test.json";
-import { Button, Row, Title } from "./tools.components";
+import { Button, Row, Title, Profile } from "./tools.components";
 
 class App extends Component {
   constructor(props) {
@@ -11,21 +11,55 @@ class App extends Component {
       accepted: false
     };
   }
+
   render() {
     return (
       <div className="App">
         <header className="App-header" style={{ alignItems: "left" }}>
           <div style={{ width: 700, margin: "auto" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                height: 400
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+              >
+                <Title style={{ padding: "0 0 10px 0" }}>Profile Pic</Title>
+                <Profile src={this.info.ProfilePic} alt={"Russia"} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+              >
+                <Title style={{ padding: "0 0 10px 0" }}>ID</Title>
+                <Profile
+                  src={this.info.IDPic}
+                  alt={":("}
+                  style={{ borderRadius: "15px" }}
+                />
+              </div>
+            </div>
             <Title>Personal Info</Title>
             <Row>
-              <p style={{ padding: "0px 0 0px 0" }}>
+              <p style={{ padding: "2px 0 0px 0" }}>
                 {this.info.Name}, {this.info.Email}, {this.info.Phone}
               </p>
             </Row>
             <Title>Emergency Contacts</Title>
             <Row>
               <p style={{ fontWeight: "600", fontSize: "20px" }}>Contact 1: </p>
-              <p style={{ padding: "3px 0 0 10px" }}>
+              <p style={{ padding: "2px 0 0 0px" }}>
                 {this.info.EName1}, {this.info.EPhone1}, {this.info.EMail1}
               </p>
             </Row>
@@ -48,7 +82,7 @@ class App extends Component {
               <p style={{ fontWeight: "600", fontSize: "20px" }}>
                 Reference 1:
               </p>
-              <p style={{ padding: "3px 0 0 10px" }}>
+              <p style={{ padding: "2px 0 0 0px" }}>
                 {this.info.R1Name}, {this.info.R1Phone}, {this.info.R1Email}
               </p>
             </Row>
@@ -56,7 +90,7 @@ class App extends Component {
               <p style={{ fontWeight: "600", fontSize: "20px" }}>
                 Reference 2:
               </p>
-              <p style={{ padding: "3px 0 0 10px" }}>
+              <p style={{ padding: "2px 0 0 0px" }}>
                 {this.info.R2Name}, {this.info.R2Phone}, {this.info.R2Email}
               </p>
             </Row>
--- src/tools.components.js
+++ src/tools.components.js
@@ -24,11 +24,38 @@ export const Row = styled.div`
   font-size: 18px;
   display: flex;
   font-weight: 200;
+
+  // prettier-ignore
+  p { margin: 20px;  }
 `;
 
 export const Title = styled.div`
+  margin-top: 12px;
   font-size: 24px;
   display: flex;
   align-items: "left";
   font-weight: 600;
+`;
+
+const PROFILE_SIZE = 325;
+export const Profile = styled.img`
+  width: ${PROFILE_SIZE}px;
+  height: ${PROFILE_SIZE}px;
+
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border-radius: 50%;
+  color: white;
+`;
+export const ID = styled.div`
+  width: 250px;
+  height: 50px;
+  border-radius: 150px;
+  border: solid 1px #0a2b3f;
+  background-image: url(${props => props.background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  overflow: hidden;
 `;
